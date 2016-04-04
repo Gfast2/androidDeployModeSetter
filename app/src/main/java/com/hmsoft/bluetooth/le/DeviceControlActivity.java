abe31d41796c6b4bb6c3430b278633ed0d463f04
Su <Gfast2@hotmail.com>
2016-04-04 18:22:35 +0200

    figure out small details

--- app/src/main/java/com/hmsoft/bluetooth/le/DeviceControlActivity.java
+++ app/src/main/java/com/hmsoft/bluetooth/le/DeviceControlActivity.java
@@ -142,7 +142,7 @@ public class DeviceControlActivity extends Activity {
         // Sets up UI references.
         mDataField = (TextView) findViewById(R.id.data_value);
         edtSend = (EditText) this.findViewById(R.id.edtSend);
-        edtSend.setText("www.jnhuamao.cn");
+        edtSend.setText("message to sent");
         svResult = (ScrollView) this.findViewById(R.id.svResult);
         
         btnSend = (Button) this.findViewById(R.id.btnSend);
